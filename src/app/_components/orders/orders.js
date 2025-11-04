c0b5dfcea9ce1cd3b859df6b843801108bb4240b
25LP4789 25LP4789 <25LP4789@Setups-iMac.local>
2025-11-04 12:32:11 +0800

    1

--- src/app/_components/orders/orders.js
+++ src/app/_components/orders/orders.js
@@ -1,15 +1,31 @@
 const Orders = () => {
   return (
     <>
-      <div className="flex bg-white text-black w-[80%]">
-        <input type="checkbox" />
-        <p>1</p>
-        <p>email</p>
-        <p>2 foods</p>
-        <p>2025/12/12</p>
-        <p>$123</p>
-        <p>Address</p>
-        <p>Pending</p>
+      <div className="flex justify-around bg-white text-black w-[80%]">
+        <div className=" w-[80]">
+          <input type="checkbox" />
+        </div>
+        <div className=" w-[80]">
+          <p>1</p>
+        </div>
+        <div className=" w-[200]">
+          <p>email</p>
+        </div>
+        <div className=" w-[80]">
+          <p>2 foods</p>
+        </div>
+        <div className=" w-[100]">
+          <p>2025/12/12</p>
+        </div>
+        <div className=" w-[80]">
+          <p>$123</p>
+        </div>
+        <div className=" w-[200]">
+          <p>Address</p>
+        </div>
+        <div className=" w-[80]">
+          <p>Pending</p>
+        </div>
       </div>
     </>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,18 @@
+import { Table, TableBody } from "@/components/ui/table";
 import Nav from "./_components/nav/left-nav";
 import OrderNav from "./_components/orders/order-nav";
 import Orders from "./_components/orders/orders";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-gray-300 h-screen">
       <Nav />
       <div className="relative flex flex-col top-20 left-60">
         <OrderNav />
-        <Orders/>
+        <Orders />
+        <Button className="bg-amber-50 text-black w-[100]">123</Button>
       </div>
-      
     </div>
   );
 }
